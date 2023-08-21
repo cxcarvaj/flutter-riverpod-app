7dba38c7e7257f5ac48f59342bd276bb56bb3f0b
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-08-21 14:52:35 -0500

    feat(riverpod_app): Stream provider

---
+++ lib/presentation/providers/stream_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stream_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$usersInChatHash() => r'8ef95e232f31895c54b9cf9e1a1b6c2911f63498';
+
+/// See also [usersInChat].
+@ProviderFor(usersInChat)
+final usersInChatProvider = AutoDisposeStreamProvider<List<String>>.internal(
+  usersInChat,
+  name: r'usersInChatProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$usersInChatHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UsersInChatRef = AutoDisposeStreamProviderRef<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
