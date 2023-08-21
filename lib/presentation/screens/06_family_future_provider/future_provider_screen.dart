893a6b3d3d93efcb5483c353b67f8e5a59e1b34c
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-08-21 12:54:08 -0500

    feat(riverpod_app): Future providers with dependecies and with arguments

--- lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class FamilyFutureScreen extends StatelessWidget {
+class FamilyFutureScreen extends ConsumerStatefulWidget {
   const FamilyFutureScreen({super.key});
 
   @override
+  FamilyFutureScreenState createState() => FamilyFutureScreenState();
+}
+
+class FamilyFutureScreenState extends ConsumerState<FamilyFutureScreen> {
+  int pokemonId = 3;
+  @override
   Widget build(BuildContext context) {
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Family Future Provider'),
+        title: Text('Pokemon Id: $pokemonId'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (pokemonName) => Text(pokemonName),
+          loading: () => const CircularProgressIndicator(),
+          error: (error, stackTrace) => Text(error.toString()),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'next-btn',
+            child: const Icon(Icons.plus_one_outlined),
+            onPressed: () {
+              pokemonId++;
+              setState(() {});
+            },
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            heroTag: 'previous-btn',
+            child: const Icon(Icons.exposure_minus_1_outlined),
+            onPressed: () {
+              if (pokemonId > 1) {
+                pokemonId--;
+              }
+              setState(() {});
+            },
+          )
+        ],
       ),
     );
   }
